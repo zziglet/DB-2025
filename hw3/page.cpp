f3ad05d2429595eb651bea4778a00f9dcc85eaad
Jiwon Jeong <jjw04033@konkuk.ac.kr>
2025-06-14 20:56:31 +0900

    feat: add hw3 split function

--- hw3/page.cpp
+++ hw3/page.cpp
@@ -1,70 +1,129 @@
 #include "page.hpp"
-#include <iostream> 
-#include <cstring> 
+#include <iostream>
+#include <cstring>
 
-void put2byte(void *dest, uint16_t data){
-	*(uint16_t*)dest = data;
+#define STRING_LEN 20
+
+void put2byte(void *dest, uint16_t data)
+{
+	*(uint16_t *)dest = data;
 }
 
-uint16_t get2byte(void *dest){
-	return *(uint16_t*)dest;
+uint16_t get2byte(void *dest)
+{
+	return *(uint16_t *)dest;
 }
 
-page::page(uint16_t type){
+page::page(uint16_t type)
+{
 	hdr.set_num_data(0);
-	hdr.set_data_region_off(PAGE_SIZE-1-sizeof(page*));
-	hdr.set_offset_array((void*)((uint64_t)this+sizeof(slot_header)));
+	hdr.set_data_region_off(PAGE_SIZE - 1 - sizeof(page *));
+	hdr.set_offset_array((void *)((uint64_t)this + sizeof(slot_header)));
 	hdr.set_page_type(type);
 }
 
-uint16_t page::get_type(){
+uint16_t page::get_type()
+{
 	return hdr.get_page_type();
 }
 
-uint16_t page::get_record_size(void *record){
+uint16_t page::get_record_size(void *record)
+{
 	uint16_t size = *(uint16_t *)record;
 	return size;
 }
 
-char *page::get_key(void *record){
-	char *key = (char *)((uint64_t)record+sizeof(uint16_t));
+char *page::get_key(void *record)
+{
+	char *key = (char *)((uint64_t)record + sizeof(uint16_t));
 	return key;
 }
 
-uint64_t page::get_val(void *key){
-	uint64_t val= *(uint64_t*)((uint64_t)key+(uint64_t)strlen((char*)key)+1);
+uint64_t page::get_val(void *key)
+{
+	uint64_t val = *(uint64_t *)((uint64_t)key + (uint64_t)strlen((char *)key) + 1);
 	return val;
 }
 
-void page::set_leftmost_ptr(page *p){
+void page::set_leftmost_ptr(page *p)
+{
 	leftmost_ptr = p;
 }
 
-page *page::get_leftmost_ptr(){
-	return leftmost_ptr;	
+page *page::get_leftmost_ptr()
+{
+	return leftmost_ptr;
 }
 
-uint64_t page::find(char *key) {
+uint64_t page::find(char *key)
+{
 	// Please implement this function in project 2.
-	
-	// offset arr의 시작 지점부터 체크
+
+	// 1. offset arr의 시작 지점부터 체크
 	void *offset_array = hdr.get_offset_array();
 	int num_data = hdr.get_num_data();
 
-	for (int i = 0; i < num_data; i++) {
+	// 2-1. Leaf page(기존 코드)
+	if (get_type() == LEAF)
+	{
+		for (int i = 0; i < num_data; i++)
+		{
 			uint16_t off = *(uint16_t *)((uint64_t)offset_array + i * 2);
 			void *record_ptr = (void *)((uint64_t)this + off);
 			char *stored_key = get_key(record_ptr);
-			
-			//인자로 받은 key와 일치하는 것이 있는지 체크
-			if (strcmp(stored_key, key) == 0) {
-					return get_val(stored_key);
+
+			// 2-1-1. 인자로 받은 key와 일치하는 것이 있는지 체크
+			if (strcmp(stored_key, key) == 0)
+			{
+				return get_val(stored_key);
 			}
+		}
+
+		// 2-1-2. 존재하지 않음
+		return 0;
 	}
+
+	// 2-2. Internal page(내부 페이지)
+	for (int i = 0; i < num_data; i++)
+	{
+		uint16_t off = *(uint16_t *)((uint64_t)offset_array + i * 2);
+		void *record_ptr = (void *)((uint64_t)this + off);
+		char *stored_key = get_key(record_ptr);
+
+		// 인자로 받은 key와 비교
+		if (strcmp(key, stored_key) < 0)
+		{
+			// 2-2-1. key가 더 작다면, leftmost 자식노드로 이동
+			if (i == 0)
+			{
+				return (uint64_t)get_leftmost_ptr();
+			}
+			else
+			{
+				// 2-2-2. key가 더 크다면
+				uint16_t prev_off = *(uint16_t *)((uint64_t)offset_array + (i - 1) * 2);
+				void *prev_record_ptr = (void *)((uint64_t)this + prev_off);
+				char *prev_key = get_key(prev_record_ptr);
+				return get_val(prev_key);
+			}
+		}
+	}
+
+	// rightmost 자식노드로 이동
+	if (num_data > 0)
+	{
+		uint16_t last_off = *(uint16_t *)((uint64_t)offset_array + (num_data - 1) * 2);
+		void *last_record_ptr = (void *)((uint64_t)this + last_off);
+		char *last_key = get_key(last_record_ptr);
+		return get_val(last_key);
+	}
+
+	// 2-2-2. 존재하지 않음
 	return 0;
 }
 
-bool page::insert(char *key, uint64_t val) {
+bool page::insert(char *key, uint64_t val)
+{
 	// Please implement this function in project 2.
 
 	// 1. record_size 계산 : record size(2byte = 16bit) + key 길이(null 포함)+ value(8byte = 64bit)
@@ -72,11 +131,12 @@ bool page::insert(char *key, uint64_t val) {
 	uint16_t record_size = sizeof(uint16_t) + key_len + sizeof(uint64_t);
 
 	// 2. space 여유 공간 체크 => 공간 없으면 insert 종료(exit point)
-	if (is_full(record_size)) return false;
+	if (is_full(record_size))
+		return false;
 
 	// 3. insert offset 계산 후 pointer 설정
 	uint16_t insert_off = hdr.get_data_region_off() - record_size + 1;
-	void *record_ptr = (void *)((uint64_t)this + insert_off); 
+	void *record_ptr = (void *)((uint64_t)this + insert_off);
 
 	// 4. 레코드 크기 저장, key/value 저장
 	put2byte(record_ptr, record_size);
@@ -87,16 +147,18 @@ bool page::insert(char *key, uint64_t val) {
 	void *offset_array = hdr.get_offset_array();
 	int num_data = hdr.get_num_data();
 	int insert_idx = 0;
-	for (; insert_idx < num_data; insert_idx++) {
-			uint16_t off = *(uint16_t *)((uint64_t)offset_array + insert_idx * 2);
-			void *r = (void *)((uint64_t)this + off);
-			if (strcmp(key, get_key(r)) < 0) break;
+	for (; insert_idx < num_data; insert_idx++)
+	{
+		uint16_t off = *(uint16_t *)((uint64_t)offset_array + insert_idx * 2);
+		void *r = (void *)((uint64_t)this + off);
+		if (strcmp(key, get_key(r)) < 0)
+			break;
 	}
 
 	// 6. 새로 삽입하기 위해 저장할 공간 밀어서 만들고 삽입
 	memmove((uint8_t *)offset_array + (insert_idx + 1) * 2,
 					(uint8_t *)offset_array + insert_idx * 2,
-					(num_data - insert_idx) * 2); //메모리 영역 겹쳐도 안전하게 이동
+					(num_data - insert_idx) * 2); // 메모리 영역 겹쳐도 안전하게 이동
 	put2byte((uint8_t *)offset_array + insert_idx * 2, insert_off);
 
 	// 7. slot header 업데이트
@@ -106,75 +168,128 @@ bool page::insert(char *key, uint64_t val) {
 	return true;
 }
 
-page* page::split(char *key, uint64_t val, char** parent_key){
+page *page::split(char *key, uint64_t val, char **parent_key)
+{
 	// Please implement this function in project 3.
-	page *new_page;
+
+	// 1. 현재 페이지와 동일한 타입의 새 페이지 생성
+	page *new_page = new page(this->get_type());
+
+	uint16_t num = hdr.get_num_data();				 // 현재 페이지의 레코드 수
+	uint32_t mid = (num + 1) / 2;							 // 절반 기준 인덱스 계산
+	void *offset_arr = hdr.get_offset_array(); // offset array 포인터
+
+	// 2. internal node : 중앙값을 상위로 올리므로 해당 키는 복사하지 않는다
+	if (new_page->get_type() != LEAF)
+	{
+		mid--;
+	}
+
+	// 3. mid 이후의 데이터를 새 페이지로 복사
+	for (int i = mid; i < num; ++i)
+	{
+		uint16_t offset = *(uint16_t *)((uint64_t)offset_arr + i * 2);
+		void *record_ptr = (void *)((uint64_t)this + offset);
+		char *k = (char *)(get_key(record_ptr));
+		uint64_t v = get_val((void *)(get_key(record_ptr)));
+		new_page->insert(k, v);
+	}
+
+	// 4. 상위 노드로 올릴 parent_key 설정 (새 페이지의 첫 번째 key 사용)
+	uint16_t split_off = *(uint16_t *)((uint64_t)offset_arr + mid * 2);
+	void *split_record = (void *)((uint64_t)this + split_off);
+	char *split_k = get_key(split_record);
+	*parent_key = new char[STRING_LEN];
+	strcpy(*parent_key, split_k);
+
+	hdr.set_num_data(num + 1);
+	defrag();
+
+	// 5. 새로운 key를 적절한 페이지에 삽입
+	if (strcmp(key, get_key(*parent_key)) < 0)
+	{
+		insert(key, val);
+	}
+	else
+	{
+		new_page->insert(key, val);
+	}
+
+	// 6. internal node : leftmost_ptr를 적절히 설정
+	if (this->get_type() != LEAF)
+	{
+		new_page->set_leftmost_ptr(get_leftmost_ptr());					// 새 페이지는 기존의 leftmost 자식 포인터 유지
+		set_leftmost_ptr((page *)get_val((void *)*parent_key)); // 현재 페이지는 parent_key에 해당하는 자식으로 변경
+	}
+
 	return new_page;
 }
 
-bool page::is_full(uint64_t inserted_record_size) {
+bool page::is_full(uint64_t inserted_record_size)
+{
 	// Please implement this function in project 2.
 
 	uint16_t data_off = hdr.get_data_region_off();
 	uint16_t offset_array_start = sizeof(slot_header);
 	uint16_t offset_array_used = hdr.get_num_data() * sizeof(uint16_t);
-
 	uint16_t available_space = data_off - (offset_array_start + offset_array_used) + 1;
 	uint16_t required_space = inserted_record_size + sizeof(uint16_t);
-
 	return available_space < required_space;
 }
 
-void page::defrag(){
+void page::defrag()
+{
 	page *new_page = new page(get_type());
 	int num_data = hdr.get_num_data();
-	void *offset_array=hdr.get_offset_array();
-	void *stored_key=nullptr;
-	uint16_t off=0;
-	uint64_t stored_val=0;
-	void *data_region=nullptr;
+	void *offset_array = hdr.get_offset_array();
+	void *stored_key = nullptr;
+	uint16_t off = 0;
+	uint64_t stored_val = 0;
+	void *data_region = nullptr;
 
-	for(int i=0; i<num_data/2; i++){
-		off= *(uint16_t *)((uint64_t)offset_array+i*2);	
-		data_region = (void *)((uint64_t)this+(uint64_t)off);
+	for (int i = 0; i < num_data / 2; i++)
+	{
+		off = *(uint16_t *)((uint64_t)offset_array + i * 2);
+		data_region = (void *)((uint64_t)this + (uint64_t)off);
 		stored_key = get_key(data_region);
-		stored_val= get_val((void *)stored_key);
-		new_page->insert((char*)stored_key,stored_val);
-	}	
+		stored_val = get_val((void *)stored_key);
+		new_page->insert((char *)stored_key, stored_val);
+	}
 	new_page->set_leftmost_ptr(get_leftmost_ptr());
 
 	memcpy(this, new_page, sizeof(page));
-	hdr.set_offset_array((void*)((uint64_t)this+sizeof(slot_header)));
+	hdr.set_offset_array((void *)((uint64_t)this + sizeof(slot_header)));
 	delete new_page;
-
 }
 
-void page::print(){
+void page::print()
+{
 	uint32_t num_data = hdr.get_num_data();
-	uint16_t off=0;
-	uint16_t record_size= 0;
-	void *offset_array=hdr.get_offset_array();
-	void *stored_key=nullptr;
-	uint64_t stored_val=0;
+	uint16_t off = 0;
+	uint16_t record_size = 0;
+	void *offset_array = hdr.get_offset_array();
+	void *stored_key = nullptr;
+	uint64_t stored_val = 0;
 
 	printf("## slot header\n");
-	printf("Number of data :%d\n",num_data);
+	printf("Number of data :%d\n", num_data);
 	printf("offset_array : |");
-	for(int i=0; i<num_data; i++){
-		off= *(uint16_t *)((uint64_t)offset_array+i*2);	
-		printf(" %d |",off);
+	for (int i = 0; i < num_data; i++)
+	{
+		off = *(uint16_t *)((uint64_t)offset_array + i * 2);
+		printf(" %d |", off);
 	}
 	printf("\n");
 
-	void *data_region=nullptr;
-	for(int i=0; i<num_data; i++){
-		off= *(uint16_t *)((uint64_t)offset_array+i*2);	
-		data_region = (void *)((uint64_t)this+(uint64_t)off);
+	void *data_region = nullptr;
+	for (int i = 0; i < num_data; i++)
+	{
+		off = *(uint16_t *)((uint64_t)offset_array + i * 2);
+		data_region = (void *)((uint64_t)this + (uint64_t)off);
 		record_size = get_record_size(data_region);
 		stored_key = get_key(data_region);
-		stored_val= get_val((void *)stored_key);
+		stored_val = get_val((void *)stored_key);
 		printf("==========================================================\n");
-		printf("| data_sz:%u | key: %s | val :%lu | key_len:%lu\n",record_size,(char*)stored_key, stored_val,strlen((char*)stored_key));
-
+		printf("| data_sz:%u | key: %s | val :%lu | key_len:%lu\n", record_size, (char *)stored_key, stored_val, strlen((char *)stored_key));
 	}
 }
